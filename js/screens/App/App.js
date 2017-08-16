39b83e190d3f8dd0f71cfd3cb6e6f71aab18996e
Chirag Singh Toor <chirag@Chirags-MacBook-Pro.local>
2017-08-15 20:43:23 -0700

    remove capture code-push test

--- js/screens/App/App.js
+++ js/screens/App/App.js
@@ -160,11 +160,11 @@ class App extends Component {
                   </TouchableOpacity>
                 </View>
                 <View style={{flex: 1, alignItems: 'center', marginBottom: 30}}>
-                  <TouchableOpacity onPress={this.takePicture}>
+                  {/*<TouchableOpacity onPress={this.takePicture}>
                     <View style={styles.captureBorder}>
                       <View style={styles.captureButton} />
                     </View>
-                  </TouchableOpacity>
+                  </TouchableOpacity>*/}
                 </View>
                 <View style={{flex: 1, alignItems: 'flex-end', paddingRight: 20}}>
                   {previewCount > 0 ?
